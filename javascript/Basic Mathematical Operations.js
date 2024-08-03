6bbf66f345ae84b33fda426d89db611137d36bcd
sheep-keeper <157627931+sheep-keeper@users.noreply.github.com>
2024-08-03 12:09:23 -0500

    complete basic mathematical operations

---
+++ javascript/Basic Mathematical Operations.js
@@ -0,0 +1,26 @@
+//Your task is to create a function that does four basic mathematical operations.
+
+//The function should take three arguments - operation(string/char), value1(number), value2(number).
+//The function should return result of numbers after applying the chosen operation.
+
+
+function basicOp(operation, value1, value2){
+    let total
+    if (operation ==="+"){
+      total = value1 + value2
+    }
+      else if (operation ==="-"){
+        total = value1 - value2
+  
+      }
+      else if (operation ==="*"){
+        total = value1 * value2
+      }
+      else if (operation ==="/"){
+        total = value1 / value2
+      }
+      else{
+          return "this is not a valid operation."
+      }
+    return total;
+  }
